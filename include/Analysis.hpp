e65f76b9687e7bffd2e0915e0e5dd5935ebd1aaa
Liza Maykova <elizavetabmstu@yandex.ru>
2020-12-27 16:23:39 +0300

    lab04

--- include/Analysis.hpp
+++ include/Analysis.hpp
@@ -5,10 +5,44 @@
 #ifndef EXP_ANALYSIS_HPP
 #define EXP_ANALYSIS_HPP
 
+#include <string>
+#include <vector>
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <utility>
+#include <algorithm>
 
-class Analysis {
+using boost::filesystem::path;
+using boost::filesystem::filesystem_error;
+using boost::filesystem::directory_iterator;
+//using boost::
+using boost::filesystem::directory_entry;
 
+struct account{
+    std::vector<std::string> balance;
+    uint32_t file_count;
+    std::string acc_id;
+    std::string bal_id;
+    std::string last_date;
 };
 
+class Analysis
+{
+public:
+    Analysis(std::string& dirPath);
+    ~Analysis();
+    void Analyze();
+    void AccPush(const account& tempAcc);
+    void FileAnalyze(const path& filePath);
+    void PathAnalyze(const directory_entry& dirEntry);
+    void DirAnalyze(const directory_entry& dirPath);
+    bool NameAnalyze(const path& filePath);
+    bool isNumber(const std::string& fileNamePart);
+    void OutPut();
+private:
+    std::vector<struct account> _trader;
+    path _path;
+    directory_iterator _dirIterator;
+};
 
 #endif //EXP_ANALYSIS_HPP
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,25 @@
 //
 // Created by lizer on 27.12.2020.
 //
+#include "Analysis.hpp"
+#include <iostream>
 
+int main(int argc, char* argv[]) {
+    std::string path;
+    if (argc == 0)
+    {
+        path = ".";
+        Analysis exp(path);
+        exp.Analyze();
+        exp.OutPut();
+    }
+
+    if (argc == 2)
+    {
+        path = argv[1];
+        Analysis exp(path);
+        exp.Analyze();
+        exp.OutPut();
+    }
+
+}

07c22501fdf7e73970aadf8830c5edecd8322ad6
Liza Maykova <elizavetabmstu@yandex.ru>
2020-12-28 17:14:08 +0300

    endend

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,25 @@
-#include <header.hpp>
+//
+// Created by lizer on 27.12.2020.
+//
+#include "Analysis.hpp"
+#include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::string path;
+    if (argc == 0)
+    {
+        path = ".";
+        Analysis exp(path);
+        exp.Analyze();
+        std::cout << exp;
+    }
 
+    else if (argc == 2)
+    {
+        path = argv[1];
+        Analysis exp(path);
+        exp.Analyze();
+        std::cout << exp;
+    }
+    return 0;
 }
